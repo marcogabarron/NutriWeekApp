27461fe160735062a99c9d7c49f4c38dfbee6061
Jessica <jessicaoliveira3108@gmail.com>
2015-07-06 16:53:26 -0300

    animação no edit

--- NutriWeekApp/SelectedCollectionViewCell.swift
+++ NutriWeekApp/SelectedCollectionViewCell.swift
@@ -17,15 +17,4 @@ class SelectedCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var image: UIImageView!
     
-    
-    
-//    @IBAction func changeLabelText(sender: AnyObject) {
-//        if(self.nada == false){
-//            self.viewCell.backgroundColor = UIColor.greenColor()
-//        }else{
-//            self.viewCell.backgroundColor = UIColor.redColor()
-//        }
-//        self.nada = !self.nada
-//        
-//    }
 }
