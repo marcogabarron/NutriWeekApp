f059d64c802d49eec1626ba55efa39f7d8d5e42f
Gabarron <Gabarron@MacBook-Pro-de-Gabarron.local>
2015-07-06 14:30:47 -0300

    Itens inseridos no cardapio

--- NutriWeekApp/CollectionVC.swift
+++ NutriWeekApp/CollectionVC.swift
@@ -17,8 +17,8 @@ class CollectionVC: UIViewController, UICollectionViewDelegate, UICollectionView
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        Array = ["Pao de Queijo", "Morango", "Leite", "Uva"]
-        ArrayImages = ["cheesebread", "strawberry.jpg", "milk.jpg", "grape.jpg"]
+        Array = ["Pao de Queijo", "Morango", "Leite", "Uva", "Feijão", "Ovo Cozido", "Batata Cozida", "Cenoura", "Barra de Cereal", "Queijo", "Frango Grelhado", "Alface", "Bife", "Suco de Laranja", "Chá de Pessego", "Tomate", "Arroz Branco", "Água"]
+        ArrayImages = ["cheesebread.jpg", "strawberry.jpg", "milk.jpg", "grape.jpg", "bean.jpg", "boiledegg.jpg", "boiledpotato.jpg", "carrot.jpg", "cerealbar.jpg", "cheese.jpg", "grilledckicken.jpg", "lettuce.jpg", "met.jpg", "orangejuice.jpg", "peachtea.jpg", "tomato.jpg", "whiterice.jpg", "water.jpg"]
         
     }
 
